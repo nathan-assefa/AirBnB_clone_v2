6ea436f560b44076706b17d962e8c6377204bcf0
nathan-assefa <nnathanassefa@gmail.com>
2023-03-20 06:00:14 -0700

    AirBnb clone_v2

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,4 +7,4 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- Granting all privileges on the database
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- Granting SELECT privileges on the database
-GRANT SELECT ON performance_schema.* TO 'hbnb_testt'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
