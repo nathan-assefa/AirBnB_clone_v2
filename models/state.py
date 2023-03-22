cf081f93814cdab531d4591fd1338d8fc315d64c
nathan-assefa <nnathanassefa@gmail.com>
2023-03-22 07:54:19 -0700

    AirBnb clone_v2

--- models/state.py
+++ models/state.py
@@ -1,47 +1,35 @@
 #!/usr/bin/python3
-""" This script defines the class State that
-inherit from the Base class in order to have
-the built in functionalites of the sqlachemy """
+"""This is the state class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
-from os import environ
+from sqlalchemy import Column, Integer, String
+import models
+from models.city import City
+import shlex
 
 
 class State(BaseModel, Base):
-    """ Defining a state class """
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
-    __tablename__ = 'states'
-
-    if environ['HBNB_TYPE_STORAGE'] == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship(
-                'City', cascade='all, delete', backref='state'
-                )
-
-    else:
-        @property
-        def cities(self):
-            """ This getter function gets all the city having
-            retation with a State instance via the primary and
-            foreign key relationship.
-            Since this is one to many relatinship,
-            only one state can be mapped to multiple cities.
-            This is done by compairing City.state_id == self.id
-            self in this case demonistrate the current instance of
-            the State class
-            """
-
-            from models import storage
-            #first we need to fetch all the city from the database
-            all_city = storage.all(City)
-            fitered_city = []
-
-            for city in all_city.values():
-                if city.state_id == self.id:
-                    filtered_city.append(city)
-
-            return filterd_city
-
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
