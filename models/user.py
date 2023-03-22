77dd9f282de291871634069b670d82008a7cc508
nathan-assefa <nnathanassefa@gmail.com>
2023-03-22 04:55:19 -0700

    AirBnb clone_v2

--- models/user.py
+++ models/user.py
@@ -6,10 +6,10 @@ from sqlalchemy import Column, String
 
 class User(BaseModel, Base):
     """ A user class """
-    
+
     __tablename__ = 'users'
-    
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
