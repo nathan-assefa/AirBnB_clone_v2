bca7834fcb07ee15301d8678c7ae69cdb9411643
nathan-assefa <nnathanassefa@gmail.com>
2023-03-22 07:27:57 -0700

    AirBnb clone_v2

--- models/place.py
+++ models/place.py
@@ -6,20 +6,6 @@ from sqlalchemy.orm import relationship, backref
 from os import getenv
 
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    place_amenity = Table("place_amenity", Base.metadata,
-                          Column("place_id",
-                                 String(60),
-                                 ForeignKey("places.id"),
-                                 primary_key=True,
-                                 nullable=False),
-                          Column("amenity_id",
-                                 String(60),
-                                 ForeignKey("amenities.id"),
-                                 primary_key=True,
-                                 nullable=False))
-
-
 class Place(BaseModel, Base):
     """This is the class for place
     Attributes:
@@ -70,13 +56,7 @@ class Place(BaseModel, Base):
             cascade="all,delete",
             backref=backref("place", cascade="all,delete"),
             passive_deletes=True)
-
-        amenities = relationship(
-            "Amenity",
-            secondary="place_amenity",
-            viewonly=False,
-            back_populates="place_amenities")
-
+        
     else:
         @property
         def reviews(self):
@@ -86,16 +66,3 @@ class Place(BaseModel, Base):
             from models import storage
             return {k: v for k, v in storage.all().items()
                     if v.place_id == self.id}
-
-        @property
-        def amenities(self):
-            """returns list of amenity ids"""
-            from models import storage
-            return self.amenity_ids
-
-        @amenities.setter
-        def amenities(self, obj):
-            """appends amenity id to amenity_ids"""
-            # from models import Amenity
-            if type(obj) is Amenity and obj.id not in self.amenity_ids:
-                self.amenity_ids.append(obj.id)
