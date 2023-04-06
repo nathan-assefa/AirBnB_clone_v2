5312da025acc6151305ea81673d93c4be1421e1b
nathan-assefa <nnathanassefa@gmail.com>
2023-04-05 23:11:05 -0700

    AirBnb clone_v2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static
-# Install Nginx if it not already installed
-if [ ! -x /usr/sbin/nginx ]
-then
-    sudo apt-get -y update
-    sudo apt-get -y install nginx
-fi
-# Create the folders
-sudo mkdir -p /data/web_static/releases/test/
+# Writing a Bash script that sets up your web servers for the deployment of web_static
+
+#installing nginx
+
+sudo apt-get -y update
+sudo apt-get -y install nginx
+
+# Creating differnt folders
 sudo mkdir -p /data/web_static/shared/
-# Create a fake HTML file
-touch /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test/
+
+# Writing into a file
+# echo "<h3>Hi everyone, this is Nathan</h3>" > sudo tee /data/web_static/releases/test/index.html
 echo "<html>
   <head>
   </head>
   <body>
-    <h1>Testing Nginx configuration <h1>
+    Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
-# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder
+</html>" | sudo tee /data/web_static/releases/test/index.htm
+
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-# Give ownership of the /data/ folder to the ubuntu user AND group 
-sudo chown -R ubuntu:ubuntu /data
-sudo chmod -R 755 /data/
-# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-sudo sed -i '48 i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-# Restart nginx
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i "/listen 80 default_server;/a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+
 sudo service nginx restart
