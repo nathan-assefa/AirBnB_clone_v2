520963b43bc71b63bd35f31b865ce5865069a7dd
nathan-assefa <nnathanassefa@gmail.com>
2023-04-05 22:37:32 -0700

    AirBnb clone_v2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/env bash
-# Writing a Bash script that sets up your web servers for the deployment of web_static
-
-#installing nginx
-
-sudo apt-get -y update
+# Bash script that sets up your web servers for the deployment of web_static
+sudo apt-get update
 sudo apt-get -y install nginx
-
-# Creating differnt folders
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-
-# Creating a file
-sudo touch /data/web_static/releases/test/index.html
-
-# Writing into a file
-echo "<h3>Hi everyone, this is Nathan</h3>" > sudo tee /data/web_static/releases/test/index.html
-
+# echo "<!DOCTYPE html>\n<html>\n  <head>\n  </head>\n  <body>\n\tHolberton School\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
+echo "This is a Test!" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-
-sudo sed -i "/listen 80 default_server;/a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
-
+sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/save_default
+sudo sed -i "/server_name .*;/a location /hbnb_static {\nalias /data/web_static/current;\n}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
