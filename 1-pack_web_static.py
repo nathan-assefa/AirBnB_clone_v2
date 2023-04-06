e45a1e244e85e4d404a03b86c4ebeb5da4642648
nathan-assefa <nnathanassefa@gmail.com>
2023-04-06 01:23:33 -0700

    AirBnb clone_v2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,12 +6,12 @@ from datetime import datetime
 
 def do_pack():
     """ Generating archive file """
-    
+
     local('mkdir -p versions')
-    
+
     file_path = "versions/web_static_{}.tgz"\
         .format(datetime.strftime(datetime.now(), "%Y%m%d%I%M%S"))
-    
+
     archive_file = local("tar -cvzf {} web_static".format(file_path))
     
     if archive_file.falied:
