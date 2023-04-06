fdb4134894a8dd65fd621a450585dec82033759a
nathan-assefa <nnathanassefa@gmail.com>
2023-04-05 23:19:52 -0700

    AirBnb clone_v2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
-# Writing a Bash script that sets up your web servers for the deployment of web_static
+# setup web static server
 
-#installing nginx
-
-sudo apt-get -y update
-sudo apt-get -y install nginx
-
-# Creating differnt folders
-sudo mkdir -p /data/web_static/shared/
+sudo apt-get update
+sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 
-# Writing into a file
-# echo "<h3>Hi everyone, this is Nathan</h3>" > sudo tee /data/web_static/releases/test/index.html
 echo "<html>
   <head>
   </head>
