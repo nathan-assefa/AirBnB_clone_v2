e0780c697da14b31da308066152bc5d7bc9560d4
nathan-assefa <nnathanassefa@gmail.com>
2023-04-06 00:58:34 -0700

    AirBnb clone_v2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,9 +10,9 @@ def do_pack():
     dt = datetime.utcnow()
     file = 'versions/web_static_{}.tgz'\
         .format(datetime.strftime(datetime.now(), "%Y%m%d%I%M%S"))
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
+    #if os.path.isdir("versions") is False:
+        #if local("mkdir -p versions").failed is True:
+            #return None
     if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
     return file
