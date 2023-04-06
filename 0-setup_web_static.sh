688bd1f172ace0349ea54f036b2e0d3ac6036d42
nathan-assefa <nnathanassefa@gmail.com>
2023-04-05 23:23:03 -0700

    AirBnb clone_v2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,7 +15,7 @@ echo "<html>
 </html>" | sudo tee /data/web_static/releases/test/index.htm
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data
 
 sudo sed -i "/listen 80 default_server;/ a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
