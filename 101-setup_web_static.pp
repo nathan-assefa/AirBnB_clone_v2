3fe198852de740cee69035a2e83f9ddc360ec3b0
nathan-assefa <nnathanassefa@gmail.com>
2023-04-07 05:26:06 -0700

    AirBnb clone_v2

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,23 +1,25 @@
 # puppet manifest preparing a server for static content deployment
 
-exec {'/usr/bin/env apt-get -y update'}
--> exec {'/usr/bin/env apt-get -y install nginx'}
+exec {'/usr/bin/env apt-get -y update':}
+-> exec {'/usr/bin/env apt-get -y install nginx':}
 
--> exec {'/usr/bin/env mkdir -p /data/web_static/releases/test/'}
--> exec {'/usr/bin/env mkdir -p /data/web_static/shared/'}
+-> exec {'/usr/bin/env mkdir -p /data/web_static/releases/test/':}
+-> exec {'/usr/bin/env mkdir -p /data/web_static/shared/':}
 
--> exec {'Write Hello World in index with tee command':
-  command => '/usr/bin/env echo "Hello Wolrd Puppet" | sudo tee /data/web_static/releases/test/index.html',
+-> file {'/data/web_static/releases/test/index.html':
+ensure => present,
+content => 'Hi everyone |This is Nathan',
 }
--> exec {'Create Symbolic link':
-  command => '/usr/bin/env ln -sf /data/web_static/releases/test /data/web_static/current',
+
+-> file {'/data/web_static/current':
+    ensure => link,
+    target => '/data/web_static/releases/test/',
 }
--> exec {'Change owner and group like ubuntu':
-  command => '/usr/bin/env chown -R ubuntu:ubuntu /data',
+
+-> exec {'Inserting line':
+    command => '/usr/bin/env sed -i "/listen 80 default_server;/a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default',
 }
--> exec {'Add new configuration to NGINX':
-  command => '/usr/bin/env sed -i "/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
-}
--> exec {'Restart NGINX':
-  command => '/usr/bin/env service nginx restart',
-}
+
+-> exec {'/usr/bin/env chown -R ubuntu:ubuntu /data':}
+
+-> exec {'/usr/bin/env service nginx restart':}
