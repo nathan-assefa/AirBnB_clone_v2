f0c347f0a21070a091ed9de99c5c6620fca948f0
nathan-assefa <nnathanassefa@gmail.com>
2023-04-07 05:06:55 -0700

    AirBnb clone_v2

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,29 +1,25 @@
 # Implementing task 0 using puppet
 
 exec {'/usr/bin/env apt-get -y update'}
--> package {'nginx':
-    provider => apt,
-    ensure => installed,
-}
+-> exec {'/usr/bin/env apt-get -y install nginx'}
 
--> exec {'sudo /usr/bin/env mkdir -p /data/web_static/releases/test/'}
--> exec {'sudo /usr/bin/env mkdir -p /data/web_static/shared/'}
+-> exec {'/usr/bin/env mkdir -p /data/web_static/releases/test/'}
+-> exec {'/usr/bin/env mkdir -p /data/web_static/shared/'}
 
 -> file {'/data/web_static/releases/test/index.html':
 ensure => present,
 content => 'Hi everyone |This is Nathan',
 }
 
--> file {'Symbolic link':
-    path => '/data/web_static/current',
+-> file {'/data/web_static/current':
     ensure => link,
     target => '/data/web_static/releases/test/',
 }
 
 -> exec {'Inserting line':
-    command => 'sudo sed -i "/listen 80 default_server;/a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default',
+    command => '/usr/bin/env sed -i "/listen 80 default_server;/a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default',
 }
 
--> exec {'sudo /usr/bin/env chown -R ubuntu:ubuntu /data'}
+-> exec {'/usr/bin/env chown -R ubuntu:ubuntu /data'}
 
--> exec {'sudo /usr/bin/env service nginx restart'}
+-> exec {'/usr/bin/env service nginx restart'}
