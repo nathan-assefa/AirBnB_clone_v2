27111a6076cd73f90a2760cbcef9e729aa41e990
nathan-assefa <nnathanassefa@gmail.com>
2023-04-07 05:32:50 -0700

    AirBnb clone_v2

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -8,7 +8,7 @@ exec {'/usr/bin/env apt-get -y update':}
 
 -> file {'/data/web_static/releases/test/index.html':
 ensure => present,
-content => 'Hi everyone |This is Nathan',
+content => 'echo "Hi everyone | This is Nathan" | sudo tee /data/web_static/releases/test/index.html',
 }
 
 -> file {'/data/web_static/current':
