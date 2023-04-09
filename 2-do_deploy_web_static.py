bd3a45faa6df124ab66b953feb9d37755ef5ccd6
nathan-assefa <nnathanassefa@gmail.com>
2023-04-08 21:13:05 -0700

    AirBnb clone_v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,10 +17,9 @@ def do_deploy(archive_path):
     if path_existence is False:
         return False
     try:
-        
         file_with_ext = archive_path.split('/')[-1]
         file_without_ext = archive_path.split('.')[0]
-        folder = '/data/web_static/releases/{}/'.format(file_with_ext)
+        folder = '/data/web_static/releases/{}'.format(file_with_ext)
         put(archive_path, '/tmp/')
         run('mkdir -p {}'.format(folder))
         run('tar -xzf /tmp/{} -C {}/'.format(file_with_ext, folder))
