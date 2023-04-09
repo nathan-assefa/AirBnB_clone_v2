f02df2453da3a0757d169f85a408b6d907a8ae3a
nathan-assefa <nnathanassefa@gmail.com>
2023-04-08 21:43:45 -0700

    AirBnb clone_v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,12 +24,30 @@ def do_deploy(archive_path):
         run('mkdir -p /data/web_static/releases/{}'.format(file_without_ext))
         run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(
             file_with_ext, file_without_ext))
+        # Moving the files from web_static directory
+        run('mv /data/web_static/releases/{}/web_static/* /data/web_static\
+                /releases/{}/'.format(file_without_ext, file_without_ext))
+
+        # Removing the web_static directory
+        run('rm -rf /data/web_static/releases/{}\
+                /web_static'.format(file_without_ext))
+
+        # Now we have uncompressed folder. Therefor,
+        # we can rermove the archive
         run('rm /tmp/{}'.format(file_with_ext))
-        run('mv {}/web_static/* {}'.format(folder, folder))
-        run('rm -rf {}/web_static'.format(folder))
-        current = '/data/web_static/current'
-        run('rm -rf {}'.format(current))
-        run('ln -s {}/ {}'.format(folder, current))
+
+        # Removing the symbolic link
+        run('rm -rf /data/web_static/current')
+
+        run('ln -s /data/web_static/releases/{}/ /data/\
+                web_static/current'.format(file_without_ext))
         return True
+       #run('rm /tmp/{}'.format(file_with_ext))
+       # run('mv {}/web_static/* {}'.format(folder, folder))
+       # run('rm -rf {}/web_static'.format(folder))
+       # current = '/data/web_static/current'
+       # run('rm -rf {}'.format(current))
+       # run('ln -s {}/ {}'.format(folder, current))
+       # return True
     except Exception:
         return False
