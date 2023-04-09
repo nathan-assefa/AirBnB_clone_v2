b70e54aac272b2e5f1cfcf168af5a3d192100264
nathan-assefa <nnathanassefa@gmail.com>
2023-04-09 11:52:06 -0700

    AirBnb clone_v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,52 +1,49 @@
 #!/usr/bin/python3
-"""
-Fabric script that distributes an archive to your web servers
-using the function do_deploy
-"""
-from fabric.api import env, run, put, hosts
-# from datetime import datetime
-import os
+# Fabfile to distribute an archive to a web server.
+import os.path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
-env.hosts = ['35.153.52.120', '52.91.183.3']
+env.hosts = ["52.91.183.3", "35.153.52.120"]
 
 
 def do_deploy(archive_path):
-    """ function distrubtes an archive to my web servers
+    """Distributes an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
-    path_existence = os.path.exists(archive_path)
-    if path_existence is False:
+    if os.path.isfile(archive_path) is False:
         return False
-    try:
-        file_with_ext = archive_path.split('/')[-1]
-        file_without_ext = archive_path.split('.')[0]
-        folder = '/data/web_static/releases/{}'.format(file_without_ext)
-        put(archive_path, '/tmp')
-        run('mkdir -p /data/web_static/releases/{}'.format(file_without_ext))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(
-            file_with_ext, file_without_ext) 
-        run('mv {}/web_static/* {}'.format(folder, folder))
-        # run('mv /data/web_static/releases/{}/web_static/* /data/web_static\
-        # /releases/{}/'.format(file_without_ext, file_without_ext))
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-        # Removing the web_static directory
-        run('rm -rf {}/web_static'.format(folder))
-
-        # Now we have uncompressed folder. Therefor,
-        # we can rermove the archive
-        run('rm /tmp/{}'.format(file_with_ext))
-
-        # Removing the symbolic link
-        run('rm -rf /data/web_static/current')
-
-        # run('ln -s /data/web_static/releases/{}/ /data/\
-        # web_static/current'.format(file_without_ext))
-        # return True
-        # run('rm /tmp/{}'.format(file_with_ext))
-        # run('mv {}/web_static/* {}'.format(folder, folder))
-        # run('rm -rf {}/web_static'.format(folder))
-        # current = '/data/web_static/current'
-        # run('rm -rf {}'.format(current))
-        run('ln -s {}/ /data/web_static/current'.format(folder))
-        return True
-    except Exception:
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
