7055037a22b1b1e74b764f0ebc8c07f6e4063d0e
nathan-assefa <nnathanassefa@gmail.com>
2023-04-21 11:42:45 +0300

    web_flask

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" This script starts the wsgi application and the web page
+at any ip address of 0.0.0.0 in port 5000
+"""
+from flask import Flask, render_template
+
+
+apt = Flask(__name__)
+
+
+@apt.route('/', strict_slashes=False)
+def hello():
+    """ This function simply rendesrs the string 'Hello HBNB' """
+    return ('Hello HBNB!')
+
+
+@apt.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Rendering a string 'HBNB' """
+    return 'HBNB'
+
+
+@apt.route('/c/<text>', strict_slashes=False)
+def text(text):
+    """ Rendering a variable and replacign '_' with space """
+    text = text.replace('_', ' ')
+    return 'C %s' % text
+
+
+@apt.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@apt.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@apt.route('/number/<int:n>')
+def number(n):
+    return "{} is a number".format(n)
+
+
+@apt.route('/number_template/<int:n>', strict_slashes=False)
+def num_html(n):
+    return render_template('5-number.html', number=n)
+
+
+@apt.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def even_odd(n):
+    return render_template('6-number_odd_or_even.html', number=n)
+
+
+if __name__ == "__main__":
+    apt.run(host='0.0.0.0', port=5000)
