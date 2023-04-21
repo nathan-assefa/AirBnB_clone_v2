8618c9f5c2e4c6984cb5803a7a9b85d4702650e3
nathan-assefa <nnathanassefa@gmail.com>
2023-04-21 11:02:34 +0300

    web_flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,7 +38,7 @@ def number(n):
     return "{} is a number".format(n)
 
 
-@apt.route('/number_template/<int:n>')
+@apt.route('/number_template/<int:n>', strict_slashes=False)
 def num_html(n):
     return render_template('5-number.html', number=n)
 
