0eb956b25453588d615569c53e1bca9920f36c34
nathan-assefa <aasamnew@gmail.com>
2023-05-04 09:39:27 +0000

    AirBnB_v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,10 +4,8 @@
     using the function do_deploy:
 """
 
-
 from fabric.api import run, local, put, env
 import os
-
 
 env.hosts = ["54.173.114.81", "54.210.188.25"]
 
@@ -20,22 +18,24 @@ def do_deploy(archive_path):
 
     try:
         # Setting up required parameters(follow steps of 1, 2, & 3)
-        # 1. Extracting the archive file from the pathj
+        # 1. Extracting the archive file from the path
         arch_file = archive_path.split("/")[-1]
 
-        # 2. Removing the extenssion .tar from the file
-        no_ext = "/data/web_static/releases/" + arch_file.split('.')[0]
+        # 2. Removing the extension .tar from the file
+        no_ext = "/data/web_static/releases/" + arch_file.replace(
+                ".tar.gz", ""
+                )
 
-        # 3. Adjusting the location where the archive is stored temporarly
-        remot_temp = "/tmp/{}".format(arch_file)
+        # 3. Adjusting the location where the archive is stored temporarily
+        remote_temp = "/tmp/{}".format(arch_file)
 
         # Storing the archive to the remote server using 'put' command
         put(archive_path, "/tmp/")
 
-        # Here the archive is in the remove server of '/tmp/' folder.
-        # We can uncompress the archive from the /tmp/
-        run('mkdir -p no_ext')
-        run("tar -xzf {} -C {}".format(remot_temp, no_ext))
+        # Here the archive is in the remote server of '/tmp/' folder.
+        # We can uncompress the archive from the /tmp/ location
+        run("mkdir -p {}".format(no_ext))
+        run("tar -xzf {} -C {}".format(remote_temp, no_ext))
 
         # We can now remove the archive from /tmp/ directory
         run("rm {}".format(remote_temp))
@@ -45,7 +45,7 @@ def do_deploy(archive_path):
         run("mv {}/web_static/* {}".format(no_ext, no_ext))
 
         # We can now remove the old one
-        run("rm -rf {}/web_static".fromat(no_ext))
+        run("rm -rf {}/web_static".format(no_ext))
 
         # Let us remove the old symbolic link
         run("rm /data/web_static/current")
