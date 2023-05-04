07ff649ad612c24f19a3fcce1ca265ee731149a3
nathan-assefa <aasamnew@gmail.com>
2023-05-04 06:34:24 +0000

    AirBnB_v2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,13 +8,16 @@ from datetime import datetime
 
 
 def do_pack():
-    """ Fabric script that generates a .tgz archive from the contents of the...
-    ...web_static folder """
-    local("sudo mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    """ packing web_static directory """
+    if not os.path.exists('./versions'):
+        os.makedirs('./versions')
+
+    date = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+
     filename = "versions/web_static_{}.tgz".format(date)
     result = local("sudo tar -cvzf {} web_static".format(filename))
-    if result.succeeded:
-        return filename
-    else:
+    if result.failed:
         return None
+    else:
+        return filename
+
