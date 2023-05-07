047a6fa0eb1adcf5ed44099e70c1e762c965ff96
nathan-assefa <aasamnew@gmail.com>
2023-05-07 06:44:26 +0000

    AirBnB_v2

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,33 +1,43 @@
-# puppet manifest preparing a server for static content deployment
-
-puppet apply -e '
-package { "nginx":
+# Install Nginx if not already installed
+package { 'nginx':
   ensure => installed,
 }
 
-file { ["/data", "/data/web_static", "/data/web_static/releases", "/data/web_static/shared", "/data/web_static/releases/test"]:
+# Directories to create
+$directories = [
+  '/data',
+  '/data/web_static',
+  '/data/web_static/releases',
+  '/data/web_static/shared',
+  '/data/web_static/releases/test',
+]
+
+# Create directories
+file { $directories:
   ensure  => directory,
-  owner   => "ubuntu",
-  group   => "ubuntu",
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
   recurse => true,
 }
 
-file { "/data/web_static/releases/test/index.html":
+file { '/data/web_static/releases/test/index.html':
   ensure  => file,
-  owner   => "ubuntu",
-  group   => "ubuntu",
-  content => "<html><body>Test Page</body></html>",
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  content => '<html><body>Test Page</body></html>',
 }
 
-file { "/data/web_static/current":
+# Create symbolic link
+file { '/data/web_static/current':
   ensure => link,
-  target => "/data/web_static/releases/test",
-  owner  => "ubuntu",
-  group  => "ubuntu",
+  target => '/data/web_static/releases/test',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
   force  => true,
 }
 
-file { "/etc/nginx/sites-available/default":
+# Update Nginx configuration
+file { '/etc/nginx/sites-available/default':
   ensure  => file,
   content => "
     server {
@@ -38,13 +48,13 @@ file { "/etc/nginx/sites-available/default":
 
         index index.html;
 
-        location /hbnb_static {
+        location /hbnb_static/ {
             alias /data/web_static/current;
             index index.html;
         }
     }
   ",
-  notify => Service["nginx"],
+  notify => Service['nginx'],
 }
 
 # Set ownership of /data/ directory
@@ -52,8 +62,9 @@ exec { 'set_ownership_data_directory':
   command => '/usr/bin/env chown -R ubuntu:ubuntu /data/',
 }
 
-service { "nginx":
+# Restart Nginx
+service { 'nginx':
   ensure    => running,
   enable    => true,
-  subscribe => File["/etc/nginx/sites-available/default"],
-}'
+  subscribe => File['/etc/nginx/sites-available/default'],
+}
