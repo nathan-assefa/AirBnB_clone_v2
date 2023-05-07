f2de00f512e4e8a59403d2c18a0dd73614f4fdd0
nathan-assefa <aasamnew@gmail.com>
2023-05-07 07:03:33 +0000

    AirBnB_v2

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,57 +1,24 @@
-# Install Nginx if not already installed
-package { 'nginx':
-  ensure => installed,
+# puppet manifest preparing a server for static content deployment
+
+exec {'/usr/bin/env apt-get -y update':}
+-> exec {'/usr/bin/env apt-get -y install nginx':}
+
+-> exec {'/usr/bin/env mkdir -p /data/web_static/releases/test/':}
+-> exec {'/usr/bin/env mkdir -p /data/web_static/shared/':}
+
+-> exec {'Write Hello World in index with tee command':
+  command => '/usr/bin/env echo "Hello Puppet" | sudo tee /data/web_static/releases/test/index.html',
 }
 
-# Directories to create
-$directories = [
-  '/data',
-  '/data/web_static',
-  '/data/web_static/releases',
-  '/data/web_static/shared',
-  '/data/web_static/releases/test',
-]
-
-# Create directories
-file { $directories:
-  ensure  => directory,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  recurse => true,
+-> file {'/data/web_static/current':
+    ensure => link,
+    target => '/data/web_static/releases/test/',
 }
 
-file { '/data/web_static/releases/test/index.html':
-  ensure  => file,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  content => '<html><body>Test Page</body></html>',
+-> exec {'Add new configuration to NGINX':
+  command => '/usr/bin/env sed -i "/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
 }
 
-# Create symbolic link
-file { '/data/web_static/current':
-  ensure => link,
-  target => '/data/web_static/releases/test',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  force  => true,
-}
+-> exec {'/usr/bin/env chown -R ubuntu:ubuntu /data':}
 
-# Update Nginx configuration
-exec { 'update_nginx_config':
-  command => "sudo sed -i '/root \\/var\\/www\\/html;/i \\ \\n\\tlocation \\/hbnb_static\\/ {\\n\\t\\talias \\/data\\/web_static\\/current\\/;\\n\\t}\\n' /etc/nginx/sites-available/default",
-  path    => '/usr/bin:/usr/sbin:/bin:/sbin',
-  require => File['/etc/nginx/sites-available/default'],
-  notify  => Service['nginx'],
-}
-
-# Set ownership of /data/ directory
-exec { 'set_ownership_data_directory':
-  command => '/usr/bin/env chown -R ubuntu:ubuntu /data/',
-}
-
-# Restart Nginx
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
-}
+-> exec {'/usr/bin/env service nginx restart':}
