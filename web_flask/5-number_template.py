2743183df590a6e7feafd45498675034f91eda5c
nathan-assefa <nnathanassefa@gmail.com>
2023-05-20 15:19:41 +0300

    AirBnB_clone_v2

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -8,31 +8,36 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def root():
-    return ('Hello HBNB!')
+    return "Hello HBNB!"
 
-@app.route('/hbnb', strict_slashes=False)
+
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    return ('HBNB')
+    return "HBNB"
 
-@app.route('/c/<text>')
+
+@app.route("/c/<text>")
 def c_text(text):
-    return 'C {}'.format(text.replace('_', ' '))
+    return "C {}".format(text.replace("_", " "))
 
-@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
+
+@app.route("/python", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
 def python_text(text):
-    return 'Python {}'.format(text.replace('_', ' '))
+    return "Python {}".format(text.replace("_", " "))
 
-@app.route('/number/<int:n>', strict_slashes=False)
+
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    return '{} is a number'.format(n)
+    return "{} is a number".format(n)
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    return render_template('5-number.html', number=n)
+    return render_template("5-number.html", number=n)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
