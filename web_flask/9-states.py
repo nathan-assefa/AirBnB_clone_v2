2f342c56b3a16acc98e9b569110ee5c2fe21f03b
nathan-assefa <aasamnew@gmail.com>
2023-05-21 10:19:36 +0000

    AirBnB_v2

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -17,28 +17,28 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def close_db(exit):
-    """ This context function gives back the
-    connection once request is done """
+    """This context function gives back the
+    connection once request is done"""
     storage.close()
 
 
-@app.route('/states', strict_slashes=False)
+@app.route("/states", strict_slashes=False)
 def states():
     states = storage.all(State)
-    return render_template('9-states.html', states=states)
+    return render_template("9-states.html", states=states)
 
 
-@app.route('/states/<id>', strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
 def db_app(id):
-    #this function fetches all the states from mysql database
-        s = None
-        for state in storage.all(State).values():
-            if state.id == id:
-                s = state
-                break
+    # this function fetches all the states from mysql database
+    s = None
+    for state in storage.all(State).values():
+        if state.id == id:
+            s = state
+            break
 
-        return render_template('9-states.html', **locals())
+    return render_template("9-states.html", **locals())
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
